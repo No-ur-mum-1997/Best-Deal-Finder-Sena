50b529c67b751dbe19182f692d1e63e506b0e2a5
AFHCastiblanco <afhernandez891@misena.edu.co>
2023-03-02 23:01:31 -0500

    versión 1.01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PrincipalComponent } from './Modulos/principal.component';
+import { DashboardComponent } from './Modulos/dashboard/dashboard.component';
+import { LoginComponent } from './Modulos/login/login.component';
+
+
+const routes: Routes = [
+{
+path: '', component: PrincipalComponent,
+ children: [
+  { path: 'dashboard', component: DashboardComponent },
+  { path:'', redirectTo: '/dashboard', pathMatch: 'full' },
+ ]
+},
+{ path: 'login', component: LoginComponent },
+];
+
+//const routes: Routes = [];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Estructura/header/header.component';
+import { NavComponent } from './Estructura/nav/nav.component';
+import { FooterComponent } from './Estructura/footer/footer.component';
+import { PrincipalComponent } from './Modulos/principal.component';
+import { DashboardComponent } from './Modulos/dashboard/dashboard.component';
+import { LoginComponent } from './Modulos/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
